034a3ba1a4e44cc2b95a85239249ad4cc1ee8109
munozalexm15 <munozalexm15@gmail.com>
2024-03-26 22:51:02 +0100

    improved menu and character selection flow

---
+++ Source/FreeTheme_AMunoz24/Public/StagesDataRow.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+#include "Engine/DataTable.h"
+#include "StagesDataRow.generated.h"
+
+
+/**
+ * Row con las propiedades de cada personaje: Sprites, Habilidades (si da tiempo), fuerza de los tiros, imagen, etc.
+ */
+USTRUCT()
+struct FStagesDataRow : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TWeakObjectPtr<UTexture2D> Image {nullptr};
+	
+};
+
