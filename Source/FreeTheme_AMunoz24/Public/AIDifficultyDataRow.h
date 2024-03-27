0ecf4dac93fa60f07133692c415fe1758a2a31d5
munozalexm15 <munozalexm15@gmail.com>
2024-03-27 17:24:52 +0100

    added more AI shot types, starting another AI difficulty

---
+++ Source/FreeTheme_AMunoz24/Public/AIDifficultyDataRow.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+#include "Engine/DataTable.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "AIDifficultyDataRow.generated.h"
+
+
+/**
+ * Row con las propiedades de cada personaje: Sprites, Habilidades (si da tiempo), fuerza de los tiros, imagen, etc.
+ */
+USTRUCT()
+struct FAIDifficultyDataRow : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString DifficultyName;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<UBehaviorTree> DifficultyBTAsset {nullptr};
+};
+
